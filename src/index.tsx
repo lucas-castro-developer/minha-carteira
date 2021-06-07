19191f3334f4262d8f5e6b97e60b8e47b2d3e0ee
Lucas Castro <lucas.sc98@hotmail.com>
2021-06-06 22:00:57 -0300

    Change importation

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Dashboard from "./Pages/Dashboard";
+import Dashboard from "./pages/Dashboard";
 
 ReactDOM.render(
   <React.StrictMode>
